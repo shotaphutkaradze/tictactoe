3e46d27829e4a9bd0cdc7af1dd53eb730c562019
shotaphutkaradze <shotafutk09@icloud.com>
2026-09-20 14:50:57 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const body = document.body;
+const ticTacToe = document.querySelector('.ticTacToe');
+const ticTacToeButtons = document.querySelectorAll('.ticTacToeButton');
+
+const markAsX = (button) => {
+    if(button.classList.contains('markedO') || button.classList.contains('markedX')) return;
+    button.style.backgroundImage = 'url(./images/x.svg)';
+    button.style.backgroundSize = 'cover';
+    button.style.backgroundPosition = 'center';
+    button.style.backgroundColor = 'rgb(201, 53, 49)';
+    button.classList.add('markedX');
+}
+
+const botMarkAsO = () => {
+    let x=0;
+    do {
+        x = Math.floor(Math.random()*9);
+    } while (ticTacToeButtons[x].classList.contains('markedX') || ticTacToeButtons[x].classList.contains('markedO'))
+    const randomButton = ticTacToeButtons[x];
+    randomButton.style.backgroundImage = 'url(./images/o.webp)';
+    randomButton.style.backgroundSize = 'cover';
+    randomButton.style.backgroundPosition = 'center'
+    randomButton.classList.add('markedO');
+    console.log(x);
+}
+
+const checkWin = () => {
+    return [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]].some(c => 
+        c.every(i => ticTacToeButtons[i].classList.contains('markedX')) || 
+        c.every(i => ticTacToeButtons[i].classList.contains('markedO'))
+    );
+};
+
+ticTacToeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        markAsX(button);
+        if(checkWin()) return console.log('Player wins!');
+        botMarkAsO();
+        if(checkWin()) return console.log('Computer wins!');
+    });
+});
